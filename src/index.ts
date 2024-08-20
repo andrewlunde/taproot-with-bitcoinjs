9249168f3cde66ecea961aaf3226b864709544fe
Andrew Lunde <andrew.lunde@sap.com>
2024-08-19 22:51:32 -0400

    working es2016

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,52 @@
 import {
     initEccLib,
     networks,
-    script,
     Signer,
     payments,
     crypto,
     Psbt
 } from "bitcoinjs-lib";
 import { broadcast, waitUntilUTXO } from "./blockstream_utils";
-import { ECPairFactory, ECPairAPI, TinySecp256k1Interface } from 'ecpair';
-import { Taptree } from "bitcoinjs-lib/src/types";
-import { witnessStackToScriptWitness } from "./witness_stack_to_script_witness";
+import { ECPairFactory, ECPairAPI, TinySecp256k1Interface,ECPairInterface } from 'ecpair';
+import { generateMnemonic,mnemonicToSeedSync,validateMnemonic } from 'bip39';
+import BIP32Factory, { BIP32Interface } from 'bip32';
+import * as ecc from 'tiny-secp256k1';
+
+import { input, confirm, number, select } from '@inquirer/prompts';
 
 const tinysecp: TinySecp256k1Interface = require('tiny-secp256k1');
 initEccLib(tinysecp as any);
 const ECPair: ECPairAPI = ECPairFactory(tinysecp);
 const network = networks.testnet;
+const bip32 = BIP32Factory(ecc);
 
 async function start() {
-    const keypair = ECPair.makeRandom({ network });
+
+    const mnemonic = 'mass erosion auction border truly until paper stairs blur increase object acquire';
+    const seed = mnemonicToSeedSync(mnemonic)
+    const root: BIP32Interface = bip32.fromSeed(seed,network);
+
+    var index = 0;
+    var utxopath = "m/86'/1'/0'" + "/0/"+index.toString();
+
+    console.log("utxopath:  " + utxopath);
+                        
+    var child1: BIP32Interface = root.derivePath(utxopath);
+
+    var privkey = child1.privateKey;
+
+    var keypair = null; 
+    if (privkey !== undefined) {
+        console.log("Creating keypair from mnemonic.");
+        keypair = ECPair.fromPrivateKey(privkey, { network: network });
+    } else {
+        console.log("Creating random keypair.");
+        keypair = ECPair.makeRandom({ network });
+    }
+    
+    
 
     await start_p2pktr(keypair);
-    await start_taptree(keypair);
 }
 
 async function start_p2pktr(keypair: Signer) {
@@ -34,201 +59,43 @@ async function start_p2pktr(keypair: Signer) {
         network
     });
     const p2pktr_addr = p2pktr.address ?? "";
-    console.log(`Waiting till UTXO is detected at this Address: ${p2pktr_addr}`);
 
-    const utxos = await waitUntilUTXO(p2pktr_addr)
-    console.log(`Using UTXO ${utxos[0].txid}:${utxos[0].vout}`);
+    const yes_continue = await confirm({ message: 'Continue?' });
 
-    const psbt = new Psbt({ network });
-    psbt.addInput({
-        hash: utxos[0].txid,
-        index: utxos[0].vout,
-        witnessUtxo: { value: utxos[0].value, script: p2pktr.output! },
-        tapInternalKey: toXOnly(keypair.publicKey)
-    });
+    if (!yes_continue) {
+        process.exit(1);
+    } else {
+        console.log(`Waiting till UTXO is detected at this Address: ${p2pktr_addr}`);
 
-    psbt.addOutput({
-        address: "mohjSavDdQYHRYXcS3uS6ttaHP8amyvX78", // faucet address
-        value: utxos[0].value - 150
-    });
+        const utxos = await waitUntilUTXO(p2pktr_addr);
 
-    psbt.signInput(0, tweakedSigner);
-    psbt.finalizeAllInputs();
+        if (false) {
+            console.log(`Using UTXO ${utxos[0].txid}:${utxos[0].vout}`);
 
-    const tx = psbt.extractTransaction();
-    console.log(`Broadcasting Transaction Hex: ${tx.toHex()}`);
-    const txid = await broadcast(tx.toHex());
-    console.log(`Success! Txid is ${txid}`);
-}
+            const psbt = new Psbt({ network });
+            psbt.addInput({
+                hash: utxos[0].txid,
+                index: utxos[0].vout,
+                witnessUtxo: { value: utxos[0].value, script: p2pktr.output! },
+                tapInternalKey: toXOnly(keypair.publicKey)
+            });
 
+            psbt.addOutput({
+                address: "mohjSavDdQYHRYXcS3uS6ttaHP8amyvX78", // faucet address
+                value: utxos[0].value - 150
+            });
 
-async function start_taptree(keypair: Signer) {
-    // TapTree example
-    console.log(`Running "Taptree example"`);
+            psbt.signInput(0, tweakedSigner);
+            psbt.finalizeAllInputs();
 
-    // Create a tap tree with two spend paths
-    // One path should allow spending using secret
-    // The other path should pay to another pubkey
-
-    // Make random key for hash_lock
-    const hash_lock_keypair = ECPair.makeRandom({ network });
-
-    const secret_bytes = Buffer.from('SECRET');
-    const hash = crypto.hash160(secret_bytes);
-    // Construct script to pay to hash_lock_keypair if the correct preimage/secret is provided
-    const hash_script_asm = `OP_HASH160 ${hash.toString('hex')} OP_EQUALVERIFY ${toXOnly(hash_lock_keypair.publicKey).toString('hex')} OP_CHECKSIG`;
-    const hash_lock_script = script.fromASM(hash_script_asm);
-
-    const p2pk_script_asm = `${toXOnly(keypair.publicKey).toString('hex')} OP_CHECKSIG`;
-    const p2pk_script = script.fromASM(p2pk_script_asm);
-
-    const scriptTree: Taptree = [
-        {
-            output: hash_lock_script
-        },
-        {
-            output: p2pk_script
-        }
-    ];
-
-    const hash_lock_redeem = {
-        output: hash_lock_script,
-        redeemVersion: 192,
-    };
-    const p2pk_redeem = {
-        output: p2pk_script,
-        redeemVersion: 192
-    }
-
-    const script_p2tr = payments.p2tr({
-        internalPubkey: toXOnly(keypair.publicKey),
-        scriptTree,
-        network
-    });
-    const script_addr = script_p2tr.address ?? '';
-
-    const p2pk_p2tr = payments.p2tr({
-        internalPubkey: toXOnly(keypair.publicKey),
-        scriptTree,
-        redeem: p2pk_redeem,
-        network
-    });
-
-    const hash_lock_p2tr = payments.p2tr({
-        internalPubkey: toXOnly(keypair.publicKey),
-        scriptTree,
-        redeem: hash_lock_redeem,
-        network
-    });
-
-    console.log(`Waiting till UTXO is detected at this Address: ${script_addr}`);
-    let utxos = await waitUntilUTXO(script_addr)
-    console.log(`Trying the P2PK path with UTXO ${utxos[0].txid}:${utxos[0].vout}`);
-
-    const p2pk_psbt = new Psbt({ network });
-    p2pk_psbt.addInput({
-        hash: utxos[0].txid,
-        index: utxos[0].vout,
-        witnessUtxo: { value: utxos[0].value, script: p2pk_p2tr.output! },
-        tapLeafScript: [
-            {
-                leafVersion: p2pk_redeem.redeemVersion,
-                script: p2pk_redeem.output,
-                controlBlock: p2pk_p2tr.witness![p2pk_p2tr.witness!.length - 1]
-            }
-        ]
-    });
-
-    p2pk_psbt.addOutput({
-        address: "mohjSavDdQYHRYXcS3uS6ttaHP8amyvX78", // faucet address
-        value: utxos[0].value - 150
-    });
-
-    p2pk_psbt.signInput(0, keypair);
-    p2pk_psbt.finalizeAllInputs();
-
-    let tx = p2pk_psbt.extractTransaction();
-    console.log(`Broadcasting Transaction Hex: ${tx.toHex()}`);
-    let txid = await broadcast(tx.toHex());
-    console.log(`Success! Txid is ${txid}`);
-
-    console.log(`Waiting till UTXO is detected at this Address: ${script_addr}`);
-    utxos = await waitUntilUTXO(script_addr)
-    console.log(`Trying the Hash lock spend path with UTXO ${utxos[0].txid}:${utxos[0].vout}`);
-
-    const tapLeafScript = {
-        leafVersion: hash_lock_redeem.redeemVersion,
-        script: hash_lock_redeem.output,
-        controlBlock: hash_lock_p2tr.witness![hash_lock_p2tr.witness!.length - 1]
-    };
-
-    const psbt = new Psbt({ network });
-    psbt.addInput({
-        hash: utxos[0].txid,
-        index: utxos[0].vout,
-        witnessUtxo: { value: utxos[0].value, script: hash_lock_p2tr.output! },
-        tapLeafScript: [
-            tapLeafScript
-        ]
-    });
-
-    psbt.addOutput({
-        address: "mohjSavDdQYHRYXcS3uS6ttaHP8amyvX78", // faucet address
-        value: utxos[0].value - 150
-    });
-
-    psbt.signInput(0, hash_lock_keypair);
-
-    // We have to construct our witness script in a custom finalizer
-
-    const customFinalizer = (_inputIndex: number, input: any) => {
-        const scriptSolution = [
-            input.tapScriptSig[0].signature,
-            secret_bytes
-        ];
-        const witness = scriptSolution
-            .concat(tapLeafScript.script)
-            .concat(tapLeafScript.controlBlock);
-
-        return {
-            finalScriptWitness: witnessStackToScriptWitness(witness)
+            const tx = psbt.extractTransaction();
+            console.log(`Broadcasting Transaction Hex: ${tx.toHex()}`);
+            const txid = await broadcast(tx.toHex());
+            console.log(`Success! Txid is ${txid}`);
+        } else {
+            console.log(`Found UTXO ${utxos[0].txid}:${utxos[0].vout}`);
         }
     }
-
-    psbt.finalizeInput(0, customFinalizer);
-
-    tx = psbt.extractTransaction();
-    console.log(`Broadcasting Transaction Hex: ${tx.toHex()}`);
-    txid = await broadcast(tx.toHex());
-    console.log(`Success! Txid is ${txid}`);
-
-    // We can also spend from this address without using the script tree
-
-    console.log(`Waiting till UTXO is detected at this Address: ${script_addr}`);
-    utxos = await waitUntilUTXO(script_addr)
-    console.log(`Trying the Hash lock spend path with UTXO ${utxos[0].txid}:${utxos[0].vout}`);
-
-    const key_spend_psbt = new Psbt({ network });
-    key_spend_psbt.addInput({
-        hash: utxos[0].txid,
-        index: utxos[0].vout,
-        witnessUtxo: { value: utxos[0].value, script: script_p2tr.output! },
-        tapInternalKey: toXOnly(keypair.publicKey),
-        tapMerkleRoot: script_p2tr.hash
-    });
-    key_spend_psbt.addOutput({
-        address: "mohjSavDdQYHRYXcS3uS6ttaHP8amyvX78", // faucet address
-        value: utxos[0].value - 150
-    });
-    // We need to create a signer tweaked by script tree's merkle root
-    const tweakedSigner = tweakSigner(keypair, { tweakHash: script_p2tr.hash });
-    key_spend_psbt.signInput(0, tweakedSigner);
-    key_spend_psbt.finalizeAllInputs();
-
-    tx = key_spend_psbt.extractTransaction();
-    console.log(`Broadcasting Transaction Hex: ${tx.toHex()}`);
-    txid = await broadcast(tx.toHex());
-    console.log(`Success! Txid is ${txid}`);
 }
 
 start().then(() => process.exit());
